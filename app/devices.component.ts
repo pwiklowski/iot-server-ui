541470fed7e6141db2b58c2ac02810db239ae35b
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-08-16 20:27:43 +0200

    Show device list

--- app/devices.component.ts
+++ app/devices.component.ts
@@ -3,11 +3,34 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
+
+
+export class Device{
+    id: string;
+    name: string;
+
+}
+
 @Component({
     selector: 'app',
-    template:`devices`,
-  directives: [ROUTER_DIRECTIVES]
-   
+    templateUrl: "app/devices.template.html",
+    directives: [ROUTER_DIRECTIVES]
 })
 export class DevicesComponent {
+    devices: Array<Device> = new Array<Device>();
+
+    constructor(private http: Http){
+        this.getDevices();
+    }
+
+    getDevices(){
+        this.http.get("/api/devices").toPromise().then(res => {
+            console.log(res.json());
+            this.devices = res.json().devices;
+        }).catch(err => {
+        
+        });
+
+    }
+
 }
