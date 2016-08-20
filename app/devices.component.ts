00ee1e9a000b62f5c152cabdc1520bc3c81f1201
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-08-20 11:12:07 +0200

    Add device details view

--- app/devices.component.ts
+++ app/devices.component.ts
@@ -1,32 +1,58 @@
 import { Component, ViewContainerRef } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import { ActivatedRoute } from '@angular/router';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { Device, DeviceValue, DeviceVariable } from './models.ts';
+import { Subscription } from 'rxjs/Subscription';
+import { Pipe } from '@angular/core';
 
 
 
-export class Device{
-    id: string;
-    name: string;
 
+@Pipe({
+    name: 'mapToIterable'
+})
+export class MapToIterable {
+    transform(map: {}, args: any[] = null): any {
+        if (!map)
+            return null;
+    
+        var x = Object.keys(map).map((key) => ({ 'key': key, 'value': map[key] }));
+        console.log(x);
+        return x;
+    }
 }
+
 
 @Component({
     selector: 'app',
     templateUrl: "app/devices.template.html",
+    pipes: [MapToIterable],
     directives: [ROUTER_DIRECTIVES]
 })
 export class DevicesComponent {
-    devices: Array<Device> = new Array<Device>();
+    private sub: Subscription;
+    id: string;
+    variables: Array<DeviceVariable> = new Array<DeviceVariable>();
 
-    constructor(private http: Http){
-        this.getDevices();
+    ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+            this.id = params['id'];
+            this.getValues(this.id);
+        });
     }
 
-    getDevices(){
-        this.http.get("/api/devices").toPromise().then(res => {
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+    constructor(private route: ActivatedRoute, private http: Http){ }
+
+    getValues(uuid: string){
+        this.http.get("/iot/values/"+uuid).toPromise().then(res => {
             console.log(res.json());
-            this.devices = res.json().devices;
+            this.variables = res.json();
+            console.log(this.variables[0].name);
         }).catch(err => {
         
         });
