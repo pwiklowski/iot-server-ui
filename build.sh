cdbb2b38120ff0b5a2beae20936c8ca1dd8b1011
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-10-09 13:10:04 +0200

    Build all container

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ git clone git@bitbucket.org:pawwik/libcoap.git tmp/libcoap
 git clone git@bitbucket.org:pawwik/std.git tmp/std
 git clone git@bitbucket.org:pawwik/rfm69-driver.git tmp/rfm69-driver
 
-docker-compose build iotserver
+docker-compose build
