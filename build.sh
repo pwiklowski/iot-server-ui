a5e6dbf437a4acacbedbf8112cff9d9773b9b64e
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-11-22 21:07:55 +0100

    Use github repositories

--- build.sh
+++ build.sh
@@ -12,10 +12,11 @@ rm tmp/std -rf
 rm tmp/rfm69-driver -rf
 
 
-git clone git@bitbucket.org:pawwik/iot_server.git -b oic tmp/iot_server
-git clone git@bitbucket.org:pawwik/liboic.git tmp/liboic
-git clone git@bitbucket.org:pawwik/libcoap.git tmp/libcoap
-git clone git@bitbucket.org:pawwik/std.git tmp/std
-git clone git@bitbucket.org:pawwik/rfm69-driver.git tmp/rfm69-driver
+git clone https://github.com/pwiklowski/iot-server.git -b oic tmp/iot_server
+git clone https://github.com/pwiklowski/liboic.git tmp/liboic
+git clone https://github.com/pwiklowski/libcoap.git tmp/libcoap
+git clone https://github.com/pwiklowski/lightstdlib.git tmp/std
+git clone https://github.com/pwiklowski/rfm69-driver.git tmp/rfm69-driver
+
 
 docker-compose build
