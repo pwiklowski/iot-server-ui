504566f8c88a6f2f7504bcac2812e71ef607e977
pawel.wiklowski <pawel.wiklowski@gmail.com>
2017-05-13 10:31:33 +0200

    Remove modules other than webui

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /iot_server/src/
-LD_LIBRARY_PATH=../../liboic:../../libcoap:../../qcron/src ./SmartHomeServer
--- start-mongo.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --name mongo -p 27017:27017 -v /home/pawwik/dev/iot-webui-2/mydatabase:/data/db mvertes/alpine-mongo
